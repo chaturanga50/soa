5e49881af7f57af841b5576343febeb93f5a28c1
damithkothalawala <admin@drklk.org>
2017-08-08 04:59:47 -0400

    initial

--- dep_setup.sh
+++ dep_setup.sh
@@ -2,7 +2,7 @@
 
 yum install git -y
 
-`rpm -q chef 2> &1 > /dev/null`
+`rpm -q chef 2>&1 > /dev/null`
 if [ $? -ne 0 ]
 then
 	yum install https://packages.chef.io/files/stable/chef/13.2.20/el/7/chef-13.2.20-1.el7.$(uname -p).rpm -y
