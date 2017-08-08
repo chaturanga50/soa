bac0a1ccdaac0082d19905f1a8bc9cbe5effe494
damithkothalawala <admin@drklk.org>
2017-08-08 05:15:59 -0400

    initial

--- dep_setup.sh
+++ dep_setup.sh
@@ -8,4 +8,6 @@ then
 	yum install https://packages.chef.io/files/stable/chef/13.2.20/el/7/chef-13.2.20-1.el7.$(uname -p).rpm -y
 fi
 
-chef-client -zj esb.json esb/recipes/deps.rb
+git clone https://github.com/damithkothalawala/soa.git /tmp/soa
+
+chef-client -zj /temp/soa/esb.json -r "recipe[esb::deps]"
